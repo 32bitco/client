cc2f2324514756e063298e50ca7aed9338e053e3
Esteban Borai <estebanborai@gmail.com>
2022-02-19 15:51:54 -0300

    feat: input component and index route

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let name: string;
+  export let type: 'text' | 'email' | 'password' | 'number' = 'text';
+  export let value: string = '';
+  export let placeholder: string = '';
+
+  let className: string | undefined = undefined;
+  export { className as class };
+</script>
+
+<div class="flex flex-col">
+  <div class="flex relative">
+    <input
+      id={name}
+      {name}
+      {type}
+      {value}
+      {placeholder}
+      class={className}
+      on:change
+    />
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,11 @@
-<h1>Authenticate</h1>
+<script lang="ts">
+  import Authenticate from '$lib/modules/auth/components/Authenticate.svelte';
+
+  import '$lib/styles/app.css';
+</script>
+
+<div style="background-image: url('/images/login-background.jpeg');" class="bg-gray-200 bg-no-repeat bg-cover h-screen">
+  <div class="bg-gray-900/25 h-screen">
+    <Authenticate />
+  </div>
+</div>
