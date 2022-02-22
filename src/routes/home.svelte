eb1c7da9feaa91971e39767f3776c28efdb59445
David Rodriguez <dalejorod@gmail.com>
2022-02-22 00:36:43 -0400

    Design

---
+++ src/routes/home.svelte
@@ -0,0 +1,13 @@
+<header class="h-7 ">
+    <div>
+        <h1 class="text-red-500">Nexus</h1>
+        <nav>
+            <input type="text">
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
