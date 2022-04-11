659e360b774af078f0419146fe979a40298d887d
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-04-11 18:09:39 -0400

    feat: support class, and add atributes for label

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -1,24 +1,21 @@
 <script lang="ts">
+  // props for the input
   export let type = 'text';
   export let name: string;
-  export let label: string;
   export let placeholder: string | undefined = undefined;
   export let required = false;
   export let id = '';
   export let value = '';
-  export let autocomplete = false;
+  export let autocomplete = '';
+  export let inputClass = 'mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md';
+
+  // props for the label
+  export let label = '';
+  export let labelClass = 'block text-sm font-medium text-gray-700';
+  export let labelFor = '';
 </script>
 
 <div>
-  <label for={name} class="block text-sm font-medium text-gray-700">{label}</label>
-  <input
-    {type}
-    {name}
-    {id}
-    {placeholder}
-    {required}
-    {value}
-    {autocomplete}
-    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-  />
+  <label for={labelFor} class={labelClass}>{label}</label>
+  <input {type} {name} {id} {placeholder} {required} {value} {autocomplete} class={inputClass} />
 </div>
