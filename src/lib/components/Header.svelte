42418193bde5bbee75d7285cac5af150033857cc
Esteban Borai <estebanborai@gmail.com>
2022-05-22 03:43:30 -0400

    feat: add naive layout as scaffold

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,5 @@
+<header class="flex items-center px-12 bg-white h-[64px] shadow-md">
+  <figure>
+    <img src="https://via.placeholder.com/120x40" alt="Nexus Logo" height="40" width="120" />
+  </figure>
+</header>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { onMount } from 'svelte';
 
+  import Header from '$lib/components/Header.svelte';
   import { userStore } from '$lib/stores/user';
 
   import '$lib/styles/app.css';
@@ -10,4 +11,7 @@
   });
 </script>
 
-<slot />
+<div id="nexus" class="min-h-screen min-w-screen bg-slate-100 overflow-x-hidden overflow-y-scroll">
+  <Header />
+  <slot />
+</div>
