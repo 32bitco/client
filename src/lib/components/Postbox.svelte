ab12f88e018c5e17877051cb224f15f896a314de
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-05-31 07:19:53 -0400

    fix: button color and div unnecessary

--- src/lib/components/Postbox.svelte
+++ src/lib/components/Postbox.svelte
@@ -4,18 +4,20 @@
   import Button from './Button.svelte';
 </script>
 
-<div>
-  <Card>
-    <div class="mx-4">
-      <div class="flex">
-        <img class="w-10 h-10 mr-5 rounded-lg" src="" alt="" />
-        <div class="w-full">
-          <Input name="postbox" type="text" id="postbox" placeholder="Whats on your mind?" />
-        </div>
-      </div>
-      <div class="flex justify-end">
-        <Button>Post</Button>
+<Card>
+  <div class="mx-4">
+    <div class="flex">
+      <img
+        class="w-10 h-10 mr-5 rounded-lg"
+        src="https://avatars.githubusercontent.com/u/96664108?s=200&v=4"
+        alt=""
+      />
+      <div class="w-full">
+        <Input name="postbox" type="text" id="postbox" placeholder="Whats on your mind?" />
       </div>
     </div>
-  </Card>
-</div>
+    <div class="flex justify-end">
+      <Button variant="primary">Post</Button>
+    </div>
+  </div>
+</Card>
