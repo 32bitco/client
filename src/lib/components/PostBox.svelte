1e87fa0338534ca99759d86d55c13a5aa2a0b71d
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-06-13 18:58:50 -0400

    feat: `PostBox` component to allow posts creation (#15)

---
+++ src/lib/components/PostBox.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import { getContextClient } from '@urql/svelte';
+  import { newForm } from 'manzana';
+  import * as Yup from 'yup';
+
+  import { makePostService } from '$lib/services/post';
+  import { userStore } from '$lib/stores/user';
+
+  import Avatar from './Avatar.svelte';
+  import Card from './Card.svelte';
+  import Input from './Input.svelte';
+  import Button from './Button.svelte';
+
+  import { Scope } from '$lib/graphql/schema';
+
+  const urqlClient = getContextClient();
+  const postService = makePostService(urqlClient);
+  const { handleSubmit, values, errors, isSubmitting } = newForm<{
+    content: string;
+  }>({
+    initialValues: {
+      content: ''
+    },
+    validationSchema: Yup.object({
+      content: Yup.string().required()
+    }),
+    onSubmit: async ({ content }) => {
+      try {
+        await postService.createPost({
+          content,
+          scope: Scope.Public
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  });
+
+  let customClassName: string = null;
+  export { customClassName as class };
+</script>
+
+<Card class={customClassName}>
+  <form on:submit={handleSubmit}>
+    <div class="mx-4">
+      <div class="flex">
+        <Avatar size="sm" user={$userStore?.user} />
+        <div class="w-full mx-4">
+          <Input
+            name="postbox"
+            type="text"
+            id="postbox"
+            placeholder="Whats on your mind?"
+            error={$errors.content}
+            required
+            bind:value={$values.content}
+          />
+        </div>
+      </div>
+      <div class="flex justify-end">
+        <Button type="submit" isLoading={$isSubmitting} variant="primary">Post</Button>
+      </div>
+    </div>
+  </form>
+</Card>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import { userStore } from '$lib/stores/user';
+
   import Feed from '$lib/components/Feed.svelte';
+  import PostBox from '$lib/components/PostBox.svelte';
 </script>
 
 <svelte:head>
@@ -7,5 +10,8 @@
 </svelte:head>
 
 <div class="min-w-screen grid gap-4 grid-cols-6 md:grid-cols-12">
+  {#if $userStore.user}
+    <PostBox class="mt-5 col-span-6 col-start-1 md:col-span-6 md:col-start-4" />
+  {/if}
   <Feed class="col-span-6 col-start-1 md:col-span-6 md:col-start-4" />
 </div>
