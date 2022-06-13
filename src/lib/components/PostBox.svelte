38af99247136882a39f2e6b1374a5c317f986f39
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-06-13 17:32:20 -0400

    feat: using manzana and yup

--- src/lib/components/PostBox.svelte
+++ src/lib/components/PostBox.svelte
@@ -2,6 +2,9 @@
   import { makePostService } from '$lib/services/post';
   import { userStore } from '$lib/stores/user';
 
+  import { newForm } from 'manzana';
+  import * as Yup from 'yup';
+
   import Avatar from './Avatar.svelte';
   import Card from './Card.svelte';
   import Input from './Input.svelte';
@@ -9,40 +12,52 @@
 
   import { Scope } from '$lib/graphql/schema';
 
-  let content = '';
+  const { handleSubmit, values, errors, isSubmitting } = newForm<{
+    content: string;
+  }>({
+    initialValues: {
+      content: ''
+    },
+    validationSchema: Yup.object({
+      content: Yup.string().required()
+    }),
+    onSubmit: async ({ content }) => {
+      try {
+        const postService = makePostService();
+        await postService.createPost({
+          content,
+          scope: Scope.Public
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  });
 
   let customClassName: string = null;
   export { customClassName as class };
-
-  async function createPost() {
-    if (content === '') {
-      return;
-    }
-    const postService = makePostService();
-    await postService.createPost({
-      content,
-      scope: Scope.Public
-    });
-  }
 </script>
 
 <Card class={customClassName}>
-  <div class="mx-4">
-    <div class="flex">
-      <Avatar size="sm" user={$userStore?.user} />
-      <div class="w-full">
-        <Input
-          name="postbox"
-          type="text"
-          id="postbox"
-          placeholder="Whats on your mind?"
-          required
-          bind:value={content}
-        />
+  <form on:submit={handleSubmit}>
+    <div class="mx-4">
+      <div class="flex">
+        <Avatar size="sm" user={$userStore?.user} />
+        <div class="w-full mx-4">
+          <Input
+            name="postbox"
+            type="text"
+            id="postbox"
+            placeholder="Whats on your mind?"
+            error={$errors.content}
+            required
+            bind:value={$values.content}
+          />
+        </div>
+      </div>
+      <div class="flex justify-end">
+        <Button type="submit" disabled={$isSubmitting} variant="primary">Post</Button>
       </div>
     </div>
-    <div class="flex justify-end">
-      <Button on:click={createPost} variant="primary">Post</Button>
-    </div>
-  </div>
+  </form>
 </Card>
