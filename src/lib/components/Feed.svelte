db1cc3d23aa3fbb3d02f18e25c342efb201db7fe
Esteban Borai <estebanborai@gmail.com>
2022-06-14 01:38:28 -0400

    feat: support for loading placeholder

--- src/lib/components/Feed.svelte
+++ src/lib/components/Feed.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import SmileyXEyes from 'phosphor-svelte/lib/SmileyXEyes';
-
   import Post from '$lib/components/Post.svelte';
   import classNames from 'classnames';
+  import Placeholder from 'svelte-content-placeholder';
+
+  import Card from './Card.svelte';
 
   import type { OperationResultStore } from '@urql/svelte/dist/types/common';
 
@@ -16,7 +18,16 @@
 
 <ul class={className}>
   {#if $posts.fetching}
-    Please wait...
+    <li class="pt-4">
+      <Card class="h-[122px]">
+        <Placeholder width="100%">
+          <rect x="2" y="2" rx="8" ry="8" width="63" height="62" />
+          <rect x="75" y="3" rx="8" ry="8" width="149" height="22" />
+          <rect x="77" y="41" rx="8" ry="8" width="110" height="19" />
+          <rect x="2" y="72" rx="8" ry="8" width="394" height="19" />
+        </Placeholder>
+      </Card>
+    </li>
   {:else}
     {#each $posts.data.feed.feed.edges.map(({ node }) => node) as post}
       <li>
